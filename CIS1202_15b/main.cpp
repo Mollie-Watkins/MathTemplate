058958d78371e2b722429b6b81e5cd12e8cc2729
Mollie <molliewatkins388@gmail.com>
2025-11-29 23:26:08 -0500

    Final edits to clean things up a bit

--- CIS1202_15b/main.cpp
+++ CIS1202_15b/main.cpp
@@ -8,17 +8,25 @@
 using namespace std;
 
 
-int half(int);
-
-
 // Name: half
-// Purpose: calculates half of a parameter value, given any data type
-// Parameters: value - the value used un the calculation
+// Purpose: calculates half of the parameter value for floating point data types
+// Parameters: value - the value used in the calculation
 // Returns: the calculated value
 template <typename T>
 T half(T value)
 {
     return value / 2;
+}
+
+
+
+// Name: half
+// Purpose: calculates half of an int parameter value
+// Parameters: value - the int used in the calculation
+// Returns: the calculated int value
+int half(int value)
+{
+    return static_cast<int>(round(static_cast<float>(value) / 2));
 }
 
 
@@ -33,21 +41,10 @@ int main()
     int c = 9;
     
     cout << "\t\tHalf tester" << endl;
-    cout << "Double value " << a << " = " << half(a) << endl;
-    cout << "Float value " << b << " = " << half(b) << endl;
-    cout << "Int value " << c << " = " << half(c) << endl;
+    cout << "Half of the double value " << a << " = " << half(a) << endl;
+    cout << "Half of the float value " << b << " = " << half(b) << endl;
+    cout << "Half of the int value " << c << " = " << half(c) << endl;
     cout << endl << endl;
     
     return EXIT_SUCCESS;
-}
-
-
-
-// Name: half
-// Purpose: calculates half of an int parameter value
-// Parameters: value - the int used in the calculation
-// Returns: the calculated int value
-int half(int value)
-{
-    return static_cast<int>(round(static_cast<float>(value) / 2));
 }
